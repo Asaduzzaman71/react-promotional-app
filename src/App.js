ed90d4a528ac8bf0108a4af0f64fe646eb293ab9
Asaduzzaman_rabby <rabby@leapinglogic.com>
2023-10-28 23:45:16 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from "./pages/admin";
 import UserList from './pages/admin/users';
 import UserCreate from './pages/admin/users/create';
+import BlogList from './pages/admin/blogs';
+import BlogCreate from './pages/admin/blogs/create';
 import './styles/dist/css/adminlte.min.css'
 import './styles/plugins/fontawesome-free/css/all.min.css'
 
@@ -14,6 +16,8 @@ function App() {
               <Route path="/admin" element={ <Dashboard /> } />
               <Route path="/admin/users" element={ <UserList /> } />
               <Route path="/admin/users/create" element={ <UserCreate /> } />
+              <Route path="/admin/blogs" element={ <BlogList /> } />
+              <Route path="/admin/blogs/create" element={ <BlogCreate /> } />
       </Routes>
     </BrowserRouter>
     
--- src/components/admin/Sidebar.js
+++ src/components/admin/Sidebar.js
@@ -1,37 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import avatar2 from '../../assets/img/user2-160x160.jpg'
 import logo from  '../../assets/img/AdminLTELogo.png'
 import { NavLink } from 'react-router-dom';
 export default function Sidebar(){
     const [activeMenu, setActiveMenu] = useState('dashboard');
-    const [activeSubMenu, setActiveSubMenu] = useState('');
     const [isCollapseMenu, setIsCollapseMenu] = useState(false)
-    const isActiveMenu = (menu, subMenu = '') => {
-        if (menu == activeMenu) {
-            setIsCollapseMenu(true)
-        } else {
-            setIsCollapseMenu(!isCollapseMenu)
+    useEffect(() => {
+        console.log('activeMenu after render', activeMenu);
+        console.log('isCollapseMenu after render', isCollapseMenu);
+    }, [activeMenu, isCollapseMenu]);
+    const openMenuItemsFor = (menu) => {
+        console.log('Clicked menu:', menu);
+        console.log('Previous activeMenu:', activeMenu);
+        if (activeMenu !== menu) {
+          setIsCollapseMenu((prevIsCollapseMenu) => !prevIsCollapseMenu);
         }
-        setActiveMenu(menu)
-        setActiveSubMenu(subMenu)
-    }
-    const isCurrent = (menu, subMenu = "") => {
-        if (menu && !subMenu) {
-            if (activeMenu == 'dashboard' && menu == 'dashboard'){
-                return true 
-            } 
-            if (activeMenu == menu && isCollapseMenu) {
-                return true
-            } else {
-                return false
-            }
-        } else if (menu && subMenu) {
-            if (activeMenu == menu && activeSubMenu == subMenu && isCollapseMenu == true) {
-                return true
-            }
-        } else {
-            return "";
-        }
+        setActiveMenu(menu);
+        console.log('Updated activeMenu:', activeMenu);
+        console.log('Updated isCollapseMenu:', isCollapseMenu);
     }
     return (
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
@@ -39,7 +25,7 @@ export default function Sidebar(){
                 <img src={logo} alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: .8}} />
                 <span className="brand-text font-weight-light">AdminLTE 3</span>
             </a>
-
+            <h6>{activeMenu}{isCollapseMenu}</h6>
             <div className="sidebar">
                 <div className="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div className="image">
@@ -51,19 +37,19 @@ export default function Sidebar(){
                 </div>
                 <nav className="mt-2">
                     <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <li className={`nav-item ${ isCurrent('dashboard') ? "menu-open" : ""}`}  onClick={()=>isActiveMenu('dashoard')}>
+                        <li className={`nav-item`} onClick={ () => openMenuItemsFor('dashboard') }>
                             <NavLink to="/admin" className='nav-link' end>
                                 <i className="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
-                                    Dashboard {isCurrent('dashboard') ? 'true' : 'false'}
+                                    Dashboard { activeMenu === 'dashboard' && isCollapseMenu ? 'true' : 'false'}
                                 </p>
                             </NavLink>
                         </li>
-                        <li className={`nav-item ${ isCurrent('user') ? "menu-open" : ""}`} >
-                            <NavLink to="/admin/users" className='nav-link' onClick={()=>isActiveMenu('user')}>        
+                        <li className={`nav-item ${  activeMenu === 'user' && isCollapseMenu ? " menu-open" : ""}`} onClick={ () => openMenuItemsFor('user') }>
+                            <NavLink to="/admin/users" className='nav-link' >        
                                 <i className="nav-icon fas fa-chart-pie"></i>
                                 <p>
-                                    Users {isCurrent('user') ? 'true' : 'false'}
+                                    Users { activeMenu === 'user' && isCollapseMenu ? 'true' : 'false'}
                                     <i className="right fas fa-angle-left"></i>
                                 </p>
                             </NavLink>
@@ -76,6 +62,29 @@ export default function Sidebar(){
                                 </li>
                                 <li className="nav-item">
                                     <NavLink to="/admin/users/create" className='nav-link'>
+                                        <i className="far fa-circle nav-icon"></i>
+                                        <p>Create</p>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className={`nav-item ${ activeMenu === 'blog' && isCollapseMenu ? "menu-open" : ""}`} onClick={ ()=> openMenuItemsFor('blog') } >
+                            <NavLink to="/admin/blogs" className='nav-link' >        
+                                <i className="nav-icon fas fa-chart-pie"></i>
+                                <p>
+                                    Blogs {  activeMenu === 'blog' && isCollapseMenu ? 'true' : 'false'}
+                                    <i className="right fas fa-angle-left"></i>
+                                </p>
+                            </NavLink>
+                            <ul className="nav nav-treeview">
+                                <li className="nav-item">
+                                    <NavLink to="/admin/blogs" className='nav-link'>
+                                        <i className="far fa-circle nav-icon"></i>
+                                        <p>List</p>
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/admin/blogs/create" className='nav-link'>
                                         <i className="far fa-circle nav-icon"></i>
                                         <p>Create</p>
                                     </NavLink>
